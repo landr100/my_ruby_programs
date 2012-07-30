7915492475ac10b6460c3c788e1335bb36146537
Robin <gowin@collective.com>
2012-07-30 12:06:03 -0400

    adding some tests

---
+++ racktest/my_sinatra.rb
@@ -0,0 +1,10 @@
+
+# my_sinatra.rb, as follows:
+
+require 'sinatra'
+require './my_middleware'
+use MyMiddleware::Hello
+get '/' do
+	"Hello Ruby participants from across the globe!"
+end
+
---
+++ racktest/my_sinatra2.rb
@@ -0,0 +1,17 @@
+
+# my_sinatra.rb, as follows:
+
+require 'sinatra'
+
+use Rack::Auth::Basic, "Restricted Area" do |user, password|
+	user == 'super' && password == 'secretsauce'
+end
+
+get '/' do
+	"This is a secret page"
+end
+
+get '/files' do
+	"Here are the secret files"
+end
+
